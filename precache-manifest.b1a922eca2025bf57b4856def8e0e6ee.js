efcbbeff986b9dbafaf859220bd658f6aca32678
awright <awrightbus@gmail.com>
2020-07-11 16:08:21 -0500

    Updates

---
+++ precache-manifest.b1a922eca2025bf57b4856def8e0e6ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bab1c9f95d0d926caaff9389ca27cc6",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "74412f228b33823e4d8b",
+    "url": "/react-calc/static/css/main.607ae81c.chunk.css"
+  },
+  {
+    "revision": "586f01d66a8ff5d36623",
+    "url": "/react-calc/static/js/2.a1da0967.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.a1da0967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74412f228b33823e4d8b",
+    "url": "/react-calc/static/js/main.fa5ef50b.chunk.js"
+  },
+  {
+    "revision": "316ae6202d6dd97efa2a",
+    "url": "/react-calc/static/js/runtime-main.ef11baa1.js"
+  }
+]);
